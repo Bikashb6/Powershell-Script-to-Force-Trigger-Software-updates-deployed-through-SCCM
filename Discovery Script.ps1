8c25a365f1d7dd14ea7835e1203bdbfa6fff7425
Bikashb6 <51827902+Bikashb6@users.noreply.github.com>
2021-02-04 21:22:49 +0530

    Re-trigger software updates through SCCM baseline
    Re-trigger failed software updates or status with "past due will be expired", "Waiting for install" and "pending verification". Refer "Steps to create configuration baseline.pdf" to configure the baseline in SCCM. 
    
    You can also run remediation script on the impacted machine to achieve the compliance.

---
+++ Discovery Script.ps1
@@ -0,0 +1,17 @@
+﻿#Discovery script
+$wmicheck=$null
+#Check whether WMI is accessible
+$wmicheck =Get-WmiObject -namespace root\cimv2 -Class Win32_BIOS -ErrorAction SilentlyContinue
+if ($wmicheck)
+{
+        # Get list of all instances of CCM_SoftwareUpdate from root\CCM\ClientSDK for missing updates
+        $TargetedUpdates= Get-WmiObject -Namespace root\CCM\ClientSDK -Class CCM_SoftwareUpdate -Filter ComplianceState=0
+        $approvedUpdates= ($TargetedUpdates |Measure-Object).count
+        $pendingpatches=($TargetedUpdates |Where-Object {$TargetedUpdates.EvaluationState -ne 8} |Measure-Object).count
+        $rebootpending=($TargetedUpdates |Where-Object {$TargetedUpdates.EvaluationState -eq 8} |Measure-Object).count
+        if ($pendingpatches -gt 0){
+                        
+                            Return(1)
+        }
+        else {Return(0) }
+}
---
+++ Remediation Script.ps1
@@ -0,0 +1,31 @@
+﻿#Remediation Script
+#Resolve past due expired and pending verification issue for software update deployment.
+#Resolve "past due expired" & "pending verification" issue for software update deployment.
+$wmicheck=$null
+$wmicheck =Get-WmiObject  -namespace root\cimv2 -Class Win32_BIOS -ErrorAction SilentlyContinue
+New-EventLog -LogName Application -Source SyncStateScript -ErrorAction SilentlyContinue
+if ($wmicheck){
+        # Get list of all instances of CCM_SoftwareUpdate from root\CCM\ClientSDK for missing updates 
+        $TargetedUpdates= Get-WmiObject  -Namespace root\CCM\ClientSDK -Class CCM_SoftwareUpdate -Filter ComplianceState=0
+        $approvedUpdates= ($TargetedUpdates |Measure-Object).count
+        $pendingpatches=($TargetedUpdates |Where-Object {$TargetedUpdates.EvaluationState -ne 8} |Measure-Object).count
+        $rebootpending=($TargetedUpdates |Where-Object {$TargetedUpdates.EvaluationState -eq 8} |Measure-Object).count
+        if ($pendingpatches -gt 0) {
+                        try { 
+                                $MissingUpdatesReformatted = @($TargetedUpdates | ForEach-Object {
+                                if($_.ComplianceState -eq 0){[WMI]$_.__PATH}})
+                                            
+                                # The following is the invoke of the CCM_SoftwareUpdatesManager.InstallUpdates with our found updates  
+                                $InstallReturn = Invoke-WmiMethod  -Class CCM_SoftwareUpdatesManager -Name InstallUpdates -ArgumentList (,$MissingUpdatesReformatted) -Namespace root\ccm\clientsdk     
+                                Write-EventLog -LogName Application -Source SyncStateScript -EventId 666 -EntryType Information -Message “Targeted Patches :$approvedUpdates,Pending patches:$pendingpatches,Reboot Pending patches :$rebootpending,initiated $pendingpatches patches for install”  
+  
+                        } 
+                        catch{
+                            Write-EventLog -LogName Application -Source SyncStateScript -EventId 667 -EntryType Information -Message “pending patches – $pendingpatches but unable to install them ,please check Further” 
+                        }
+        }else{
+ 
+            Write-EventLog -LogName Application -Source SyncStateScript -EventId 668 -EntryType Information -Message “Targeted Patches :$approvedUpdates,Pending patches:$pendingpatches,Reboot Pending patches :$rebootpending,Compliant”  
+ }
+ 
+}
